06322c55de265bee3cfad726d6472f8c4acc32ba
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-30 11:59:48 +0200

    added new cmd

---
+++ Public/Remove-GraphManager.ps1
@@ -0,0 +1,14 @@
+﻿function Remove-GraphManager {
+    [cmdletBinding(SupportsShouldProcess)]
+    param(
+        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [alias('UserID')][string] $ID,
+        [string] $UserPrincipalName
+    )
+    if ($ID) {
+        $URI = "/users/$ID/manager/`$ref"
+    } else {
+        $URI = "/users/$UserPrincipalName/manager/`$ref"
+    }
+    Invoke-Graph -Uri $URI -Method DELETE -Headers $Headers
+}
