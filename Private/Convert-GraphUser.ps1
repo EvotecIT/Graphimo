70a59bac773a6e19f2212f7d128db35858b18790
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-12 22:56:22 +0100

    Converts extension attributtes

---
+++ Private/Convert-GraphUser.ps1
@@ -0,0 +1,36 @@
+﻿function Convert-GraphInternalUser {
+    <#
+    .SYNOPSIS
+    Converts user returned by graph with onPremisesExtensionAttributes to new simplified object
+
+    .DESCRIPTION
+    Converts user returned by graph with onPremisesExtensionAttributes to new simplified object
+
+    .PARAMETER InputObject
+    The object to convert
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][PSCustomObject] $InputObject
+    )
+    $InputObject | ForEach-Object {
+        $NewObject = [ordered] @{}
+        $Object = $_
+        foreach ($Property in $Object.PSObject.Properties.Name) {
+            if ($Property -eq 'onPremisesExtensionAttributes') {
+                foreach ($ExtensionAttribute in $Object.onPremisesExtensionAttributes.PSObject.Properties.Name) {
+                    $NewObject[$ExtensionAttribute] = $Object.onPremisesExtensionAttributes.$ExtensionAttribute
+                }
+            } else {
+                $NewObject[$Property] = $Object.$Property
+            }
+        }
+        [PSCustomObject] $NewObject
+    }
+}
