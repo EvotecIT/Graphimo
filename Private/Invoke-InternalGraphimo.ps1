25fb74248c572de6956f4ded4cb0810d4ca90e3c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-11 20:39:34 +0200

    Add internal command

---
+++ Private/Invoke-InternalGraphimo.ps1
@@ -0,0 +1,27 @@
+﻿function Invoke-InternalGraphimo {
+    [CmdletBinding()]
+    param(
+        [Array] $OutputQuery,
+        [int] $First,
+        [int] $CurrentCount
+    )
+    if ($OutputQuery.value) {
+        $FoundUsers = $OutputQuery.value
+    }
+    if ($First) {
+        if ($CurrentCount) {
+            $First = $First - $CurrentCount
+        }
+        if ($FoundUsers.Count -eq $First) {
+            return $FoundUsers
+        } elseif ($FoundUsers.Count -gt $First) {
+            $FoundUsers = $FoundUsers | Select-Object -First $First
+            return $FoundUsers
+        } else {
+            $First = $First - $FoundUsers.Count
+            $FoundUsers
+        }
+    } else {
+        $FoundUsers
+    }
+}
