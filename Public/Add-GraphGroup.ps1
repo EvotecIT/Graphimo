efabfd826ead0a54c7868b659d3d72c99fc3f85b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-11 20:39:24 +0200

    Add a group

---
+++ Public/Add-GraphGroup.ps1
@@ -0,0 +1,39 @@
+﻿function Add-GraphGroup {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [string] $DisplayName,
+        [string] $Name,
+        [string] $Description,
+        [string] $MailNickname,
+        [switch] $SecurityEnabled,
+        [switch] $MailEnabled
+    )
+    $URI = "/groups"
+    $BaseUri = 'https://graph.microsoft.com/v1.0'
+    $Body = [ordered]@{
+        groupTypes = @()
+    }
+
+    if ($PSBoundParameters.ContainsKey('DisplayName')) {
+        $Body['displayName'] = $DisplayName
+    }
+    if ($PSBoundParameters.ContainsKey('Name')) {
+        $Body['name'] = $Name
+    }
+    if ($PSBoundParameters.ContainsKey('Description')) {
+        $Body['description'] = $Description
+    }
+    if ($PSBoundParameters.ContainsKey('MailNickname')) {
+        $Body['mailNickname'] = $MailNickname
+    }
+    if ($PSBoundParameters.ContainsKey('SecurityEnabled')) {
+        $Body['securityEnabled'] = $SecurityEnabled.IsPresent
+    }
+    if ($PSBoundParameters.ContainsKey('MailEnabled')) {
+        $Body['mailEnabled'] = $MailEnabled.IsPresent
+    }
+    if ($Body.Count -gt 0) {
+        Invoke-Graphimo -Uri $URI -Method POST -Headers $Headers -Body $Body -BaseUri $BaseUri
+    }
+}
