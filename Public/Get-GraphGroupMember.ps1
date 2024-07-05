cc1c16d7ceb2d8102b6e12dc185824b984113bed
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-05 14:16:15 +0200

    Support for MgGraph

--- Public/Get-GraphGroupMember.ps1
+++ Public/Get-GraphGroupMember.ps1
@@ -1,25 +1,39 @@
 ﻿function Get-GraphGroupMember {
     [cmdletBinding()]
     param(
-        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [alias('Authorization')][System.Collections.IDictionary] $Headers,
         [parameter(Mandatory)][string] $Id,
         [string] $Search,
-        [string[]] $Property
+        [string[]] $Property,
+        [switch] $MgGraph
     )
+
+    if (-not $MgGraph -and -not $Headers -and $Script:MgGraphAuthenticated -ne $true) {
+        Write-Warning -Message "No headers or MgGraph switch provided. Skipping."
+        return
+    }
+
     if ($ID) {
         # Query a single group
         $RelativeURI = "/groups/$ID/members"
         $QueryParameter = @{
             '$Select' = $Property -join ','
             '$Search' = $Search
-
-        }
-        if ($QueryParameter.'$Search') {
-            # This is required for search to work
-            # https://developer.microsoft.com/en-us/identity/blogs/build-advanced-queries-with-count-filter-search-and-orderby/
-            $Headers['ConsistencyLevel'] = 'eventual'
         }
     }
     Remove-EmptyValue -Hashtable $QueryParameter
-    Invoke-Graphimo -Uri $RelativeURI -Method GET -Headers $Headers -QueryParameter $QueryParameter
+    $invokeGraphimoSplat = @{
+        Uri            = $RelativeURI
+        Method         = 'GET'
+        Headers        = $Headers
+        QueryParameter = $QueryParameter
+        MgGraph        = $MgGraph.IsPresent
+    }
+    if ($QueryParameter.'$Search') {
+        # This is required for search to work
+        # https://developer.microsoft.com/en-us/identity/blogs/build-advanced-queries-with-count-filter-search-and-orderby/
+        $invokeGraphimoSplat['ConsistencyLevel'] = 'eventual'
+    }
+
+    Invoke-Graphimo @invokeGraphimoSplat
 }
