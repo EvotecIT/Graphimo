322c8071b231aba31913a045f97f750870f30b08
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-05 14:16:44 +0200

    Support for MgGraph

--- Public/Remove-GraphUser.ps1
+++ Public/Remove-GraphUser.ps1
@@ -1,14 +1,21 @@
 ﻿function Remove-GraphUser {
     [cmdletBinding(SupportsShouldProcess)]
     param(
-        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter()][alias('Authorization')][System.Collections.IDictionary] $Headers,
         [string] $UserPrincipalName,
-        [alias('UserID')][string] $ID
+        [alias('UserID')][string] $ID,
+        [switch] $MgGraph
     )
+
+    if (-not $MgGraph -and -not $Headers -and $Script:MgGraphAuthenticated -ne $true) {
+        Write-Warning -Message "No headers or MgGraph switch provided. Skipping."
+        return
+    }
+
     if ($ID) {
         $URI = "/users/$ID"
     } else {
         $URI = "/users/$UserPrincipalName"
     }
-    Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers
+    Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers -MgGraph:$MgGraph.IsPresent
 }
