28b99eefedbe8137e740588c2cd3bb9f4e54fc3d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-05 14:16:37 +0200

    Support for MgGraph

--- Public/Remove-GraphGroupMember.ps1
+++ Public/Remove-GraphGroupMember.ps1
@@ -1,9 +1,9 @@
 ﻿function Remove-GraphGroupMember {
     [cmdletBinding(SupportsShouldProcess)]
     param(
-        [parameter(Mandatory, ParameterSetName = 'All')]
-        [parameter(Mandatory, ParameterSetName = 'PerID')]
-        [parameter(Mandatory, ParameterSetName = 'BySearch')]
+        [parameter(ParameterSetName = 'All')]
+        [parameter(ParameterSetName = 'PerID')]
+        [parameter(ParameterSetName = 'BySearch')]
         [alias('Authorization')][System.Collections.IDictionary] $Headers,
         [parameter(Mandatory, ParameterSetName = 'All')]
         [parameter(Mandatory, ParameterSetName = 'PerID')]
@@ -14,26 +14,31 @@
 
         [parameter(Mandatory, ParameterSetName = 'BySearch')][string] $Search,
 
-        [parameter(ParameterSetName = 'All')][switch] $All
+        [parameter(ParameterSetName = 'All')][switch] $All,
+        [switch] $MgGraph
     )
+
+    if (-not $MgGraph -and -not $Headers -and $Script:MgGraphAuthenticated -ne $true) {
+        Write-Warning -Message "No headers or MgGraph switch provided. Skipping."
+        return
+    }
+
     if ($All) {
         # Lets remove all, but to do that we need to know who to remove
-        $Users = Get-GraphGroupMember -Id $ID -Headers $Headers -Verbose -Property id, displayName
+        $Users = Get-GraphGroupMember -Id $ID -Headers $Headers -Verbose -Property id, displayName -MgGraph:$MgGraph.IsPresent
         foreach ($User in $Users) {
             $URI = "/groups/$ID/members/$($User.id)/`$ref"
-            $Status = Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers
-            $Status
+            Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers
         }
     } elseif ($Search) {
-        $Users = Get-GraphGroupMember -Id $ID -Headers $Headers -Verbose -Property id, displayName -Search $Search
+        $Users = Get-GraphGroupMember -Id $ID -Headers $Headers -Verbose -Property id, displayName -Search $Search -MgGraph:$MgGraph.IsPresent
         foreach ($User in $Users) {
             $URI = "/groups/$ID/members/$($User.id)/`$ref"
-            $Status = Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers
-            $Status
+            Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers -MgGraph:$MgGraph.IsPresent
         }
     } else {
         # Lets delete just one record
         $URI = "/groups/$ID/members/$MemberID/`$ref"
-        Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers
+        Invoke-Graphimo -Uri $URI -Method DELETE -Headers $Headers -MgGraph:$MgGraph.IsPresent
     }
 }
