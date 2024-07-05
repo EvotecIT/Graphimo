83912b34232d0b7e0497e5433d5b3627e36d6042
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-05 14:15:47 +0200

    Support for MgGraph

--- Public/Add-GraphGroupMember.ps1
+++ Public/Add-GraphGroupMember.ps1
@@ -1,10 +1,16 @@
 ﻿function Add-GraphGroupMember {
-    [cmdletBinding(SupportsShouldProcess)]
+    [cmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Headers')]
     param(
-        [parameter(Mandatory)][alias('Authorization')][System.Collections.IDictionary] $Headers,
+        [parameter()][alias('Authorization')][System.Collections.IDictionary] $Headers,
         [parameter(Mandatory)][alias('GroupID')][string] $ID,
-        [parameter(Mandatory)][string] $MemberID
+        [parameter(Mandatory)][string] $MemberID,
+        [parameter()][switch] $MgGraph
     )
+
+    if (-not $MgGraph -and -not $Headers -and $Script:MgGraphAuthenticated -ne $true) {
+        Write-Warning -Message "No headers or MgGraph switch provided. Skipping."
+        return
+    }
 
     $URI = "/groups/$ID/members/`$ref"
 
@@ -12,5 +18,5 @@
         "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$MemberID"
     }
 
-    Invoke-Graphimo -Uri $URI -Method POST -Headers $Headers -Body $Body
+    Invoke-Graphimo -Uri $URI -Method POST -Headers $Headers -Body $Body -MgGraph:$MgGraph.IsPresent
 }
